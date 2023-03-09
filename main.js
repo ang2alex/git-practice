680c549b86cc508a95aa2d1291ab06aab10931f0
ang2alex <a.karg@yandex.ru>
2023-03-09 09:41:29 +0300

    Rebased squash commit

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+// sum
 export function sum(a, b) {
   return a + b;
 }
 
+// substract
 export function subtract(a, b) {
   return a - b
+}
+
+// multiply
+export function multiply(a, b) {
+  return a * b
 }
